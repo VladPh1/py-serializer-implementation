d7683692ad09e99f241ca8bec59e8ff34e716e51
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-29 18:04:43 +0300

    Solution2

--- car/serializers.py
+++ car/serializers.py
@@ -5,13 +5,17 @@ from car.models import Car
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    manufacturer = serializers.CharField(max_length=64, read_only=True)
-    model = serializers.CharField(max_length=64, read_only=True)
-    horse_powers = serializers.IntegerField(read_only=True)
-    is_broken = serializers.BooleanField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        min_value=1,
+        max_value=1914,
+    )
+    is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(
         required=False,
-        allow_null=True
+        allow_null=True,
+        allow_blank=True,
     )
 
     def create(self, validated_data):
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from car.serializers import CarSerializer
 
 def serialize_car_object(car: Car) -> bytes:
     serialized_car = CarSerializer(instance=car).data
-    bytes_car = json.dumps(serialized_car)
-    return bytes_car
+    result_str = json.dumps(serialized_car, separators=(',', ':'))
+    return result_str.encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    data_dict = json.loads(json)
-    deserialize_car = CarSerializer(data=data_dict)
+def deserialize_car_object(json_bytes: bytes) -> Car:
+    json_string = json_bytes.decode("utf-8")
+    dict_object = json.loads(json_string)
+    deserialize_car = CarSerializer(data=dict_object)
     deserialize_car.is_valid(raise_exception=True)
-    deserialize_car.save()
-    return deserialize_car
+    return deserialize_car.save()
