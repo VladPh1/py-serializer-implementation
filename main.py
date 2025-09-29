914cc12e98e3f92be5f9a95213faf433de3e8df8
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-29 16:35:14 +0300

    Solution1

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def serialize_car_object(car: Car) -> bytes:
     bytes_car = json.dumps(serialized_car)
     return bytes_car
 
+
 def deserialize_car_object(json: bytes) -> Car:
     data_dict = json.loads(json)
     deserialize_car = CarSerializer(data=data_dict)
